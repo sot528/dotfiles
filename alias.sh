c983d9fcbc5c3e9c194ee6f51644654ff0c7676a
ishii-souta <souta_i@waku-2.com>
2016-09-14 15:09:28 +0900

    add alias.sh

---
+++ alias.sh
@@ -0,0 +1,29 @@
+# docker
+alias d='docker'
+alias dp='docker ps'
+alias dr='docker run'
+alias dk='docker kill'
+alias dl='docker logs'
+alias dpa='docker ps -a'
+alias di='docker images'
+alias drm='docker rm `docker ps -a -q`'
+alias drmi='docker rmi'
+
+# Vagrant
+alias v='vagrant'
+
+# docker-compose
+alias dcom='docker-compose'
+alias dce='docker-compose exec'
+alias dcl='docker-compose logs'
+alias dcr='docker-compose stop && docker-compose up -d'
+
+# git
+alias gitlon='git log --oneline --no-merges'
+alias gitc='git checkout'
+alias gitlln='git log --left-right --no-merges'
+alias glon='git log --oneline --no-merges'
+
+# bundler
+alias bi='bundle exec'
+alias be='bundle exec'
